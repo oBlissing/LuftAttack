b41666c22cb4ad1da6df8ac665cc3512030c4c61
Ola Blissing <ola.blissing@gmail.com>
2015-09-26 16:05:12 +0200

    Better player movement

--- static/js/gameobjects/player.js
+++ static/js/gameobjects/player.js
@@ -7,6 +7,9 @@ class Player extends Phaser.Sprite {
         game.physics.arcade.enable(this);
 
         this.damage = 10;
+        this.topVelocity = 250;
+        this.acceleration = 25;
+        this.deceleration = 10;
 
         // this.body.setSize(30, 48, 49, 60);
         this.body.bounce.y = 0.0;
@@ -19,40 +22,40 @@ class Player extends Phaser.Sprite {
     update() {
       super.update();
 
-      //  Reset the players velocity (movement)
-      // if(this.body.velocity.x > 0)
-      //     this.body.velocity.x -= 5;
-      this.body.velocity.x = 0;
-      this.body.velocity.y = 0;
-
-      if (this.cursor.left.isDown)
-      {
+      // Handle X-movement
+      if (this.cursor.left.isDown) {
           //  Move to the left
-          this.body.velocity.x = -150;
-
-      }
-      else if (this.cursor.right.isDown)
-      {
+          if (this.body.velocity.x > -this.topVelocity)
+            this.body.velocity.x -= this.acceleration;
+      } else if (this.cursor.right.isDown) {
           //  Move to the right
-          this.body.velocity.x = 150;
-
-      }
-      else if (this.cursor.up.isDown)
-      {
-          //  Move to the right
-          this.body.velocity.y = -150;
-
-      }
-      else if (this.cursor.down.isDown)
-      {
-          //  Move to the right
-          this.body.velocity.y = 150;
-
-      }
-      else
-      {
+          if (this.body.velocity.x < this.topVelocity)
+            this.body.velocity.x += this.acceleration;
+      } else {
+        // deceleration X
+        if (this.body.velocity.x > 0)
+          this.body.velocity.x -= this.deceleration;
+        else if (this.body.velocity.x < 0)
+          this.body.velocity.x += this.deceleration;
       }
 
+      // Handle Y-movement
+      if (this.cursor.up.isDown) {
+        //  Move up
+        if (this.body.velocity.y > -this.topVelocity)
+          this.body.velocity.y -= this.acceleration;
+      }
+      else if (this.cursor.down.isDown) {
+        //  Move down
+        if (this.body.velocity.y < this.topVelocity)
+          this.body.velocity.y += this.acceleration;
+      } else {
+        // deceleration Y
+        if (this.body.velocity.y > 0)
+          this.body.velocity.y -= this.deceleration;
+        else if (this.body.velocity.y < 0)
+          this.body.velocity.y += this.deceleration;
+      }
     }
 }
 
